02bd134b40dee220caf293d17174f606c312ba31
SailajaRadjou <p.sailaja@hotmail.com>
2021-09-27 17:03:09 +0200

    Connecting React to NodeJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState} from "react";
 
 function App() {
+  const [AllPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts")
+      .then((response) => {
+        setAllPosts(response.data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {AllPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postTextMsg}</div>
+            <div className="footer">{value.userName}</div>
+          </div>  
+        );
+      })}
     </div>
   );
 }
