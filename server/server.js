2687ed6259f5f1318146e324cb0fe71c2640721b
SailajaRadjou <p.sailaja@hotmail.com>
2021-09-27 12:30:45 +0200

    installer les dependencies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-app.listen(3001, () => {
+const db = require('./models');
+
+//Routers
+const postRouter = require('./routes/posts');
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
     console.log("Server is running on port 3001");
+    });
 });
